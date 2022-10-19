d869e1a605afdf5882ff39af67025737f44b14d2
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-19 21:45:44 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-echo "Updating apt-get"
-sudo apt-get -qq update
-
-echo "Installing default-java"
-sudo apt-get -y install default-jre > /dev/null 2>&1
-sudo apt-get -y install default-jdk > /dev/null 2>&1
-
-
-echo "Installing jenkins"
-sudo apt-get -y install jenkins > /dev/null 2>&1
-sudo service jenkins start
-
-sleep 1m
-
 echo "Installing Jenkins Plugins"
 JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 echo $JENKINSPWD
