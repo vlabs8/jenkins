f20456b90b16989e143333f831519ba34af338b9
vlabs8 <114287258+vlabs8@users.noreply.github.com>
2022-10-19 21:28:58 +0300

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "Adding apt-keys"
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo "Updating apt-get"
 sudo apt-get -qq update
@@ -10,11 +12,6 @@ echo "Installing default-java"
 sudo apt-get -y install default-jre > /dev/null 2>&1
 sudo apt-get -y install default-jdk > /dev/null 2>&1
 
-echo "Installing git"
-sudo apt-get -y install git > /dev/null 2>&1
-
-echo "Installing git-ftp"
-sudo apt-get -y install git-ftp > /dev/null 2>&1
 
 echo "Installing jenkins"
 sudo apt-get -y install jenkins > /dev/null 2>&1
